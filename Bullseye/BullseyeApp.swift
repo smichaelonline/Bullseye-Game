f4971fa4da91d6bc693fcc412dbdedeebc4ac968
smichaelonline <stephaniemichael001@gmail.com>
2023-02-02 16:08:16 -0500

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Stephanie Michael on 2/2/23.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
