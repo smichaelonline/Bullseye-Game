4f187bed10715a1f0bfd73d6c2c890517baa6257
smichaelonline <stephaniemichael001@gmail.com>
2023-02-02 21:57:34 -0500

    Update slider to remove constant

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -8,22 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Text("🎯🎯🎯 \nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO:")
+    
+  @State private var alertIsvisible: Bool = false
+  @State private var sliderValue: Double = 50.0
+    
+  var body: some View {
+      VStack {
+        Text("🎯🎯🎯 \nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO:")
+            .bold()
+            .kerning(2.0)
+            .lineSpacing(4.0)
+            .font(.footnote)
+            .multilineTextAlignment(.center)
+        Text("89")
+            .kerning(-1.0)
+            .font(.largeTitle)
+            .fontWeight(.black)
+        HStack{
+            Text("1")
                 .bold()
-                .kerning(2.0)
-                .lineSpacing(4.0)
-                .font(.footnote)
-                .multilineTextAlignment(.center)
-            Text("89")
-            HStack{
-                Text("1")
-                Slider(value: .constant(50), in:1.0...100.0)
-                Text("100")
-            }
-            Button(action: {}) {
-                Text("Hit Me")
+          Slider(value: self.$sliderValue, in:1.0...100.0)
+            Text("100")
+                .bold()
+        }
+        Button(action: {
+            print("hello, swiftUI")
+            self.alertIsvisible = true
+        }) {
+            Text("Hit Me")
+                .alert("Hello there!", isPresented: $alertIsvisible) {
+                    Button("Awesome!") { }
+                } message: {
+                    Text("This is my first pop-up")
+                }
             }
         }
         .padding()
